1ae3197e6b1b9762a4653a7e9ecc2a9909aa7b66
LeoHabs <117355219+LeoHabs@users.noreply.github.com>
2023-02-22 20:22:17 +0000

    add action to open and close button
    Added action to the open and close button of the side bar for the login form on the right side plus animation to slide

--- FE/src/scripts/components/loginSideBar.js
+++ FE/src/scripts/components/loginSideBar.js
@@ -1,1 +1,11 @@
 const loginSideBarOpenBtn = document.getElementById("loginSideMenuBtn");
+const loginSideBarCloseBtn = document.getElementById("loginSideMenuCloseBtn");
+const loginSideBar = document.getElementById("loginSideBar");
+
+loginSideBarOpenBtn.addEventListener("click",() => {
+    loginSideBar.setAttribute("class","loginSideMenu-show");
+});
+
+loginSideBarCloseBtn.addEventListener("click",() => {
+    loginSideBar.setAttribute("class","loginSideMenu-hidden");
+});
