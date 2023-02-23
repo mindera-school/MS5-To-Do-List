b18ed288891b31bfe0583a6b1adeb077a802ffee
rocha <rjoao0112@gmail.com>
2023-02-23 12:37:44 +0000

    Simple Set Up of MVC project

--- BE/toDoListAPI/src/main/java/shcool/mindera/toDoListAPI/controller/UserController.java
+++ BE/toDoListAPI/src/main/java/shcool/mindera/toDoListAPI/controller/UserController.java
@@ -1,9 +1,14 @@
 package shcool.mindera.toDoListAPI.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shcool.mindera.toDoListAPI.service.UserService;
 
 @RestController
 @RequestMapping("/todo/users")
 public class UserController {
+    private final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
