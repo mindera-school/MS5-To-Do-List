cca51cfa7c385ab8dbc311f69525efda7f44ad9e
LeoHabs <117355219+LeoHabs@users.noreply.github.com>
2023-02-23 15:22:49 +0000

    format loginSideBar.js

--- FE/src/scripts/components/loginSideBar.js
+++ FE/src/scripts/components/loginSideBar.js
@@ -2,15 +2,15 @@ const loginSideBarOpenBtn = document.getElementById("loginSideMenuBtn");
 const loginSideBarCloseBtn = document.getElementById("loginSideMenuCloseBtn");
 const loginSideBar = document.getElementById("loginSideBar");
 
-loginSideBarOpenBtn.addEventListener("click",() => {
-    loginSideBar.setAttribute("class","loginSideMenu-show");
+loginSideBarOpenBtn.addEventListener("click", () => {
+    loginSideBar.setAttribute("class", "loginSideMenu-show");
 
 });
 
-loginSideBarCloseBtn.addEventListener("click",() => {
-    loginSideBar.setAttribute("class","loginSideMenu-hidden");
-    setTimeout(()=>{
-        loginSideBar.setAttribute("class","loginSideMenu-nodisplay");
-    },400);
+loginSideBarCloseBtn.addEventListener("click", () => {
+    loginSideBar.setAttribute("class", "loginSideMenu-hidden");
+    setTimeout(() => {
+        loginSideBar.setAttribute("class", "loginSideMenu-nodisplay");
+    }, 400);
 });
 
