5c630e07dc3c1b33a305726b773b8b7ed16ad206
LeoHabs <117355219+LeoHabs@users.noreply.github.com>
2023-02-28 12:37:30 +0000

    fix: search bar bugs and dark mode

--- FE/src/scripts/components/searchbar.js
+++ FE/src/scripts/components/searchbar.js
@@ -6,7 +6,6 @@ const closeSearchBar = document.getElementById("eraseSearchBarBtn");
 searchBar.addEventListener("input", () => {
     stateList.innerHTML = null;
     storageList.forEach((e) => {
-
         if (e.taskName.includes(searchBar.value) || e.description.includes(searchBar.value)) {
             createOnPg(e.taskName, e.date, e.tag, e.id);
         }
