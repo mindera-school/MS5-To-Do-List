f01ae1111aaa28e742d9ab92188b984b29d5baf9
LeoHabs <117355219+LeoHabs@users.noreply.github.com>
2023-03-14 12:27:35 +0000

    add dynamic tags list for taskpreview component

--- FE/src/App.js
+++ FE/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import TaskPreview from "./components/TaskPreview";
 
 function App() {
+  
+
   return (
     <TaskPreview title="Lavar" dueDate="12/01/2045" tagsListUrl="https://todo/task/tags" ></TaskPreview>
   );
--- FE/src/components/TaskTagsList/index.jsx
+++ FE/src/components/TaskTagsList/index.jsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
 import Tag from "../Tag";
 
+export default function TaskTagsList({ listUrl }) {
+  const [tagList, setTagList] = useState([]);
+  let allTags;
 
+  useEffect(() => {
+    fetch(listUrl)
+      .then((response) => response.json())
+      .then((response) => setTagList(response));
+  }, []);
 
-export default function TaskTagsList({listUrl}) {
-    const [tagList, setTagList] = useState([]);
-    
-    useEffect(()=>{
-        fetch(listUrl)
-        .then(response => response.json())
-        .then(response => setTagList(response));
-    },[]);
+  allTags = tagList?.map((e, i) => {
+    console.log(tagList);
+    return <Tag  key={i} tagText={e.name} color={e.color}></Tag>;
+  });
 
-    return <>
-
-    </>
-
+  return <>
+  <div className="task-tagslist">
+    {allTags}
+  </div>
+  </>;
 }
