8f5283135bdfe445c0733c33286b3f3e779b800c
LeoHabs <117355219+LeoHabs@users.noreply.github.com>
2023-03-15 20:16:43 +0000

    Update FE/src/components/TaskPreview/styled-components.js
    Co-authored-by: Joao Rocha <62727949+Rocha1313@users.noreply.github.com>

--- FE/src/components/TaskPreview/styled-components.js
+++ FE/src/components/TaskPreview/styled-components.js
@@ -75,17 +75,17 @@ export const StyledFavHeart = styled.button`
 
 
 export const StyledTaskPreview = styled.div`
-display: flex;
-position: relative;
-width: 690px;
-height: 80px;
-background-color: #8D99AE;
-border-radius: 30px;
-box-sizing: border-box;
-margin: 15px;
-padding-left: 15px;
-color: $tasks-font-color;
-font-family:Roboto;
+  display: flex;
+  position: relative;
+  width: 690px;
+  height: 80px;
+  background-color: #8D99AE;
+  border-radius: 30px;
+  box-sizing: border-box;
+  margin: 15px;
+  padding-left: 15px;
+  color: $tasks-font-color;
+  font-family:Roboto;
 `;
 
 export const NameAndDone = styled.div`
