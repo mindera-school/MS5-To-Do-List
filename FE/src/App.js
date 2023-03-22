77a0f424694f22bc2104a8b1a04b9fbb5f4976ec
LeoHabs <117355219+LeoHabs@users.noreply.github.com>
2023-03-22 12:44:52 +0000

    fixed task fetch

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import { AppContext, useCreateAppContext } from "./context";
+import { taskFetcher } from "./fetchers/fetchTasks";
 import { GlobalStyle } from "./globalStyles";
-
 
 function App() {
   const [tasksList, setTasksList] = useState([]);
@@ -10,9 +10,9 @@ function App() {
 
   //Fills the tasks state list. In the future the fetch url will be coming from the user object.
   //The rest of the structure is built down from here fully autonomously to fetch the tasks
-  /*  useEffect(() => {
-     taskFetcher(1).then((res) => setTasksList(res));
-   }, []); */
+  useEffect(() => {
+    taskFetcher(1).then((res) => setTasksList(res));
+  }, []);
 
   return (
     <>
