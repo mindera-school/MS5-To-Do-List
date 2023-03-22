292d4d2d6ba574a5be53a1e8fc7834d0d9fdd77f
rocha <rjoao0112@gmail.com>
2023-03-22 11:22:43 +0000

    Repository Query Creation

--- BE/toDoListAPI/src/main/java/school/mindera/toDoListAPI/repositories/CommentsRepository.java
+++ BE/toDoListAPI/src/main/java/school/mindera/toDoListAPI/repositories/CommentsRepository.java
@@ -2,6 +2,12 @@ package school.mindera.toDoListAPI.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import school.mindera.toDoListAPI.entities.CommentsEntity;
+import school.mindera.toDoListAPI.entities.TasksEntity;
+
+import java.util.Optional;
+import java.util.List;
 
 public interface CommentsRepository extends JpaRepository<CommentsEntity, Integer> {
+
+    Optional<List<CommentsEntity>> getCommentsByTask(TasksEntity task);
 }
--- BE/toDoListAPI/src/main/java/school/mindera/toDoListAPI/service/CommentService.java
+++ BE/toDoListAPI/src/main/java/school/mindera/toDoListAPI/service/CommentService.java
@@ -1,7 +1,18 @@
 package school.mindera.toDoListAPI.service;
 
 import org.springframework.stereotype.Service;
+import school.mindera.toDoListAPI.repositories.CommentsRepository;
+import school.mindera.toDoListAPI.repositories.TasksRepository;
 
 @Service
 public class CommentService {
+    private CommentsRepository commentsRepository;
+    private TasksRepository tasksRepository;
+
+    public CommentService(CommentsRepository commentsRepository, TasksRepository tasksRepository) {
+        this.commentsRepository = commentsRepository;
+        this.tasksRepository = tasksRepository;
+    }
+
+
 }
