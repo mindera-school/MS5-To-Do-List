bc0d5c82f7cf52194a52ad78e06bb09c56589878
Gabriel Sousa <gabriel2001@live.com.pt>
2023-03-31 12:44:21 +0100

    Update index.jsx

--- FE/src/components/AccountMenus/LoginMenu/index.jsx
+++ FE/src/components/AccountMenus/LoginMenu/index.jsx
@@ -5,53 +5,71 @@ import { useAppContext } from "../../../context";
 import { LoginDiv, UserImg } from "./styled-components";
 
 async function sendLoginInfo(data, logger) {
-	if (data.username === "" || data.password === "") {
-		return console.log("Data is empty");
-	}
+  if (data.username === "" || data.password === "") {
+    return console.log("Data is empty");
+  }
 
-	const headers = new Headers();
-	headers.append("Content-Type", "application/json");
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
 
-	await fetch("http://localhost:8086/todo/users/login", {
-		method: "POST",
-		headers: headers,
-		redirect: "follow",
-		referrerPolicy: "no-referrer",
-		body: data
-	})
-		.then(r => r.json())
-		.then(r => logger(r))
-		.catch(r => console.log(r));
+  await fetch("http://localhost:8086/todo/users/login", {
+    method: "POST",
+    headers: headers,
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(data),
+  })
+    .then((r) => r.json())
+    .then((r) => logger(r))
+    .catch((r) => console.log(r));
 }
 
 function createSendObj(username, password) {
-	return { username, password };
+  return { username, password };
 }
 
 export const LoginMenu = () => {
-	const [userContent, setUserContent] = useState("");
-	const [passwordContent, setPasswordContent] = useState("");
-	const setUser = useAppContext().setCurrentUser;
-	const setMenuType = useAppContext().setMenuType;
+  const [userContent, setUserContent] = useState("");
+  const [passwordContent, setPasswordContent] = useState("");
+  const setUser = useAppContext().setCurrentUser;
+  const setMenuType = useAppContext().setMenuType;
 
-	return (
-		<LoginDiv>
-			<button onClick={() => setMenuType("register")} >Sign Up<FiUserPlus size={25} /></button>
-			<UserImg>
-				<FaRegUser size={80} />
-			</UserImg>
-			<div>
-				<label>
-					<span>Username:</span>
-					<input type="text" value={userContent} onChange={(e) => setUserContent(e.target.value)} />
-				</label>
-				<label>
-					<span>Password:</span>
-					<input type="password" value={passwordContent} onChange={(e) => { setPasswordContent(e.target.value); }} />
-				</label>
-			</div>
-			<button onClick={() => sendLoginInfo(createSendObj(userContent, passwordContent), setUser)}>Login</button>
-		</LoginDiv>
-	);
+  return (
+    <LoginDiv>
+      <button onClick={() => setMenuType("register")}>
+        Sign Up
+        <FiUserPlus size={25} />
+      </button>
+      <UserImg>
+        <FaRegUser size={80} />
+      </UserImg>
+      <div>
+        <label>
+          <span>Username:</span>
+          <input
+            type="text"
+            value={userContent}
+            onChange={(e) => setUserContent(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Password:</span>
+          <input
+            type="password"
+            value={passwordContent}
+            onChange={(e) => {
+              setPasswordContent(e.target.value);
+            }}
+          />
+        </label>
+      </div>
+      <button
+        onClick={() =>
+          sendLoginInfo(createSendObj(userContent, passwordContent), setUser)
+        }
+      >
+        Login
+      </button>
+    </LoginDiv>
+  );
 };
-
