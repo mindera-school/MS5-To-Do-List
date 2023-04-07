ca14724aaac3dd9c7e3d2e6e150b5556383713ee
LeoHabs <117355219+LeoHabs@users.noreply.github.com>
2023-04-07 15:12:21 +0100

    fix tags

--- FE/src/components/TaskDetailsModal/index.jsx
+++ FE/src/components/TaskDetailsModal/index.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import { BiEdit, BiSave } from "react-icons/bi";
 import { IoIosAddCircleOutline, IoMdClose } from "react-icons/io";
 import { useAppContext, useTaskListContext } from "../../context.js";
-import AddCommentForm from "./AddCommentForm";
-import CommentBox from "./CommentBox";
 import taskFetcher from "../../fetchers/fetchTasks.js";
 import TagsContainer from "../TagsList/index.jsx";
+import AddCommentForm from "./AddCommentForm";
+import CommentBox from "./CommentBox";
 import {
   BoxHeader,
   CustomLabel,
@@ -23,7 +23,7 @@ import {
   SubtaskInput,
   TaskDescInput,
   TaskInfo,
-  Wrapper,
+  Wrapper
 } from "./styles";
 
 export default function TaskDetailsModal({
@@ -45,7 +45,7 @@ export default function TaskDetailsModal({
   const [subtaskTitle, setSubtaskTitle] = useState("");
   const [subtaskDate, setSubtaskDate] = useState("");
   const taskList = useTaskListContext().list;
-	const theme = useAppContext().themeMode;
+  const theme = useAppContext().themeMode;
   const tasksListContext = useTaskListContext();
   const [tagsList, setTagsList] = useState();
   const [editMode, setEditMode] = useState(false);
@@ -68,35 +68,35 @@ export default function TaskDetailsModal({
       position: 0,
     };
 
-  if (currentUser === null) {
-    addChildren(task.taskId, {
-      ...data,
-      taskId: Date.now().toString(36),
-    });
-    return;
-  }
-
-  fetch("http://localhost:8086/todo/tasks/v1", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
-  })
-    .then((r) => r.json())
-    .then((r) => {
+    if (currentUser === null) {
       addChildren(task.taskId, {
         ...data,
-        taskId: r.id,
+        taskId: Date.now().toString(36),
       });
+      return;
+    }
 
-      if (r === undefined) {
-        console.log("Couldn't add subtask");
-      }
+    fetch("http://localhost:8086/todo/tasks/v1", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
     })
-    .catch(console.log("Couldn't connect"));
+      .then((r) => r.json())
+      .then((r) => {
+        addChildren(task.taskId, {
+          ...data,
+          taskId: r.id,
+        });
+
+        if (r === undefined) {
+          console.log("Couldn't add subtask");
+        }
+      })
+      .catch(console.log("Couldn't connect"));
   }
 
   useEffect(() => {
@@ -154,83 +154,89 @@ export default function TaskDetailsModal({
       sendTags(data.tags);
     });
     taskFetcher(currentUser.userId).then((res) =>
-    tasksListContext.setTaskList(res)
-  );
+      tasksListContext.setTaskList(res)
+    );
   };
 
-	return <>
-		<Wrapper onClick={manageClose} display={display}>
-			<OuterBox theme={theme} onClick={(e) => e.stopPropagation()}>
-				<BoxHeader theme={theme}>
-					<button onClick={manageClose}><IoMdClose size={25} /></button>
-					<button onClick={() => {
-						if (isEditing) {
-							const updatedTask = createDataObj();
-							const id = task.taskId;
-							if (currentUser !== null) {
-								saveEdition(updatedTask);
-							}
-							updateTask(id, updatedTask);
-						}
-						setIsEditing(isEditing ? false : true);
-					}}>
-						{isEditing ? <BiSave size={25} /> : <BiEdit size={25}></BiEdit>}
-					</button>
-				</BoxHeader>
-				<InnerBox theme={theme}>
-					<InnerHeader theme={theme}>
-						<InnerTitle theme={theme} readOnly={isEditing ? false : true} value={title} onChange={(e) => setTitle(e.target.value)}></InnerTitle>
-						<OptionTitles theme={theme}>
-							<span>Add Sub Task</span>
-							<button><IoIosAddCircleOutline color="white" size={20} onClick={() => setIsCreateSubOpen(isCreateSubOpen ? false : true)} /></button>
-						</OptionTitles>
-						<SubtaskFormBox theme={theme} opened={isCreateSubOpen}>
-							<CustomLabel theme={theme}>
-								<span>Title of Subtask</span>
-								<SubtaskInput value={subtaskTitle} onChange={(e) => setSubtaskTitle(e.target.value)}></SubtaskInput>
-							</CustomLabel>
-							<CustomLabel theme={theme}>
-								<span>Due Date</span>
-								<input type={"date"} value={subtaskDate} onChange={(e) => setSubtaskDate(e.target.value)}></input>
-							</CustomLabel>
-							<SaveBtn theme={theme} onClick={() => {
-								setIsCreateSubOpen(false);
-								saveSubtask();
-							}}>
-								<BiSave size={25} color={"white"} />
-							</SaveBtn>
-						</SubtaskFormBox>
-					</InnerHeader>
-					<TaskInfo theme={theme}>
-						<CustomLine theme={theme}>
-							<span>Sub Tasks:</span>
-							<span>0</span>
-						</CustomLine>
-						<CustomLine theme={theme}>
-							<span>Status:</span>
-							<span>{task.isDone ? "Completed" : "Still to do"}</span>
-						</CustomLine>
-						<CustomLine theme={theme}>
-							<span>End Date:</span>
-							<DateInput theme={theme} readOnly={isEditing ? false : true}><input type="date" readOnly={isEditing ? false : true} value={date} onChange={(e) => setDate(e.target.value)}></input></DateInput>
-						</CustomLine>
-						<CustomLine theme={theme}>
-							<span>Tags:</span>
-							<TagsContainer><TaskTagsList listUrl={task.tagsURL} /><button>+ Add tag</button></TagsContainer>
-						</CustomLine>
-					</TaskInfo>
-					<HorizontalLine />
-					<DescriptionContainer theme={theme}>
-						<h2>Description</h2>
-						<TaskDescInput theme={theme} readOnly={isEditing ? false : true} value={description} onChange={(e) => setDescription(e.target.value)}></TaskDescInput>
-					</DescriptionContainer>
-					<HorizontalLine />
-					<CommentBox theme={theme} comments={taskComments} ></CommentBox>
-				</InnerBox>
-				<AddCommentForm theme={theme} taskId={task.taskId} updateComments={updateTaskComments}></AddCommentForm>
-			</OuterBox>
-		</Wrapper>
-	</>;
+  return <>
+    <Wrapper onClick={manageClose} display={display}>
+      <OuterBox theme={theme} onClick={(e) => e.stopPropagation()}>
+        <BoxHeader theme={theme}>
+          <button onClick={manageClose}><IoMdClose size={25} /></button>
+          <button onClick={() => {
+            if (isEditing) {
+              const updatedTask = createDataObj();
+              const id = task.taskId;
+              if (currentUser !== null) {
+                saveEdition(updatedTask);
+              }
+              updateTask(id, updatedTask);
+            }
+            setIsEditing(isEditing ? false : true);
+          }}>
+            {isEditing ? <BiSave size={25} /> : <BiEdit size={25}></BiEdit>}
+          </button>
+        </BoxHeader>
+        <InnerBox theme={theme}>
+          <InnerHeader theme={theme}>
+            <InnerTitle theme={theme} readOnly={isEditing ? false : true} value={title} onChange={(e) => setTitle(e.target.value)}></InnerTitle>
+            <OptionTitles theme={theme}>
+              <span>Add Sub Task</span>
+              <button><IoIosAddCircleOutline color="white" size={20} onClick={() => setIsCreateSubOpen(isCreateSubOpen ? false : true)} /></button>
+            </OptionTitles>
+            <SubtaskFormBox theme={theme} opened={isCreateSubOpen}>
+              <CustomLabel theme={theme}>
+                <span>Title of Subtask</span>
+                <SubtaskInput value={subtaskTitle} onChange={(e) => setSubtaskTitle(e.target.value)}></SubtaskInput>
+              </CustomLabel>
+              <CustomLabel theme={theme}>
+                <span>Due Date</span>
+                <input type={"date"} value={subtaskDate} onChange={(e) => setSubtaskDate(e.target.value)}></input>
+              </CustomLabel>
+              <SaveBtn theme={theme} onClick={() => {
+                setIsCreateSubOpen(false);
+                saveSubtask();
+              }}>
+                <BiSave size={25} color={"white"} />
+              </SaveBtn>
+            </SubtaskFormBox>
+          </InnerHeader>
+          <TaskInfo theme={theme}>
+            <CustomLine theme={theme}>
+              <span>Sub Tasks:</span>
+              <span>0</span>
+            </CustomLine>
+            <CustomLine theme={theme}>
+              <span>Status:</span>
+              <span>{task.isDone ? "Completed" : "Still to do"}</span>
+            </CustomLine>
+            <CustomLine theme={theme}>
+              <span>End Date:</span>
+              <DateInput theme={theme} readOnly={isEditing ? false : true}><input type="date" readOnly={isEditing ? false : true} value={date} onChange={(e) => setDate(e.target.value)}></input></DateInput>
+            </CustomLine>
+            <CustomLine theme={theme}>
+              <span>Tags:</span>
+              <TagsContainer
+                tagsList={tagsList}
+                setTagsList={setTagsList}
+                editMode={editMode}
+                taskId={task.taskId}
+                display={editMode}
+              />
+            </CustomLine>
+          </TaskInfo>
+          <HorizontalLine />
+          <DescriptionContainer theme={theme}>
+            <h2>Description</h2>
+            <TaskDescInput theme={theme} readOnly={isEditing ? false : true} value={description} onChange={(e) => setDescription(e.target.value)}></TaskDescInput>
+          </DescriptionContainer>
+          <HorizontalLine />
+          <CommentBox theme={theme} comments={taskComments} ></CommentBox>
+        </InnerBox>
+        <AddCommentForm theme={theme} taskId={task.taskId} updateComments={updateTaskComments}></AddCommentForm>
+      </OuterBox>
+    </Wrapper>
+  </>;
 }
 
 function sendTags(tags) {
