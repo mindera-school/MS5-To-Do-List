5fd79c2175b5de79187d744415c8f776db47639e
Joao Rocha <rjoao0112@gmail.com>
2023-04-14 11:13:06 +0100

    Code Service Creation

---
+++ BE/toDoListAPI/src/main/java/school/mindera/toDoListAPI/service/emailhandler/CodesService.java
@@ -0,0 +1,49 @@
+package school.mindera.toDoListAPI.service.emailhandler;
+
+import org.joda.time.DateTime;
+import org.springframework.stereotype.Service;
+import school.mindera.toDoListAPI.entities.CodesEntity;
+import school.mindera.toDoListAPI.entities.UsersEntity;
+import school.mindera.toDoListAPI.exceptions.user.EmailNotFoundException;
+import school.mindera.toDoListAPI.repositories.CodesRepository;
+import school.mindera.toDoListAPI.repositories.UsersRepository;
+import school.mindera.toDoListAPI.utils.CodeGenerator;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Optional;
+
+@Service
+public class CodesService {
+
+    private final CodesRepository codesRepository;
+    private final UsersRepository usersRepository;
+
+    public CodesService(CodesRepository codesRepository, UsersRepository usersRepository) {
+        this.codesRepository = codesRepository;
+        this.usersRepository = usersRepository;
+    }
+
+    public String createNewChangePasswordAttempt(String email){
+        String token = CodeGenerator.generateToken();
+
+        Optional<UsersEntity> user = usersRepository.findByEmail(email);
+        if(user.isEmpty()){
+            throw new EmailNotFoundException("This email is not registered");
+        }
+
+        Date timestamp = new Date();
+        DateTime expireDate = new DateTime(timestamp).plusDays(1);
+
+        CodesEntity newCode = CodesEntity.builder()
+                .code(token)
+                .userId(user.get())
+                .timestamp(timestamp)
+                .expireDate(expireDate.toDate())
+                .build();
+
+        codesRepository.save(newCode);
+
+        return token;
+    }
+}
