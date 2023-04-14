d1f40e71a02c0742661ff56f15d25a7b184dc120
Joao Rocha <rjoao0112@gmail.com>
2023-04-14 11:07:25 +0100

    Code Generator Implementation

--- BE/toDoListAPI/src/main/java/school/mindera/toDoListAPI/utils/CodeGenerator.java
+++ BE/toDoListAPI/src/main/java/school/mindera/toDoListAPI/utils/CodeGenerator.java
@@ -3,7 +3,7 @@ package school.mindera.toDoListAPI.utils;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class CodeGenerator {
-    public String generate(){
+    public static String generateToken(){
         return RandomStringUtils.random(6, true, true);
     }
 }
